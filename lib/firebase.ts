c6bfd1d0fd2335c8b0683c6d566a20c944347f3c
masaki sato <mimic52006@gmail.com>
2026-01-05 12:19:11 +0900

    Firebase設定更新

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDcT3cRv9DI4ehnoC2wFTSYNITDfXk4Gw0",
+  authDomain: "moneyroom-e5dd0.firebaseapp.com",
+  projectId: "moneyroom-e5dd0",
+  storageBucket: "moneyroom-e5dd0.firebasestorage.app",
+  messagingSenderId: "1009141662216",
+  appId: "1:1009141662216:web:1c8978c96e27c51be807bd"
 }
 
 const app = initializeApp(firebaseConfig)
